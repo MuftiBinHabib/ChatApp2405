f174b858b9071d13b7aeec000ceb92ec0ce6d1f9
MuftiBinHabib <muftibinhabib500@gmail.com>
2025-05-09 17:19:26 +0600

    added flex

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 const Home = () => {
   return (
    <div className="container">
-    <section>
+    <section className='flex'>
         <h2>ChatApp</h2>
 
         <ul>
